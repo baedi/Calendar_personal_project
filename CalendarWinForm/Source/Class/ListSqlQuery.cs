896904ac2bb6b76bd429b4c41d832dbc319cd49a
Daeil Bae <47047263+baedi@users.noreply.github.com>
2019-10-16 13:36:03 +0900

    Update Calendar WinForm (10-16 2019)
    SQL-only class change operation (static -> dynamic) (2)

--- CalendarWinForm/Source/Class/ListSqlQuery.cs
+++ CalendarWinForm/Source/Class/ListSqlQuery.cs
@@ -14,14 +14,14 @@ namespace CalendarWinForm.Source.Class {
         public string sqlCreateTable(int mode){
             if (mode == CALENDAR_MODE) return "CREATE TABLE calendarlist (year INT, month INT, day INT, sethour INT, setminute INT, text VARCHAR(21), active BOOLEAN);";
             else if (mode == ALARM_MODE) return "CREATE TABLE t_alarmlist (sethour INT, setminute INT, text VARCHAR(21), active BOOLEAN, dot_week VARCHAR(4));";
-            else return null;
+            return null;
         }
 
         // INSERT SQL               
-        public string sqlInsertValues(int mode, decimal[] date, decimal[] setTime, string text, bool alarmEnable) {
-            if (mode == CALENDAR_MODE) return $"INSERT INTO calendarlist VALUES ({date[0]}, {date[1]}, {date[2]}, {setTime[0]}, {setTime[1]}, \"{text}\", {alarmEnable}";
+        public string sqlInsertValues(int mode, decimal[] setDate, decimal[] setTime, string text, bool alarmEnable) {
+            if (mode == CALENDAR_MODE) return $"INSERT INTO calendarlist VALUES ({setDate[0]}, {setDate[1]}, {setDate[2]}, {setTime[0]}, {setTime[1]}, \"{text}\", {alarmEnable}";
             else if (mode == ALARM_MODE) return $"INSERT INTO t_alarmlist VALUES ({setTime[0]}, {setTime[1]}, \"{text}\", true, \" \");";
-            else return null;
+            return null;
         }
 
         // CALENDERLIST ALL DATA    
@@ -30,15 +30,45 @@ namespace CalendarWinForm.Source.Class {
         }
 
         // LISTVIEW REFRESH         
-        public string sqlListViewRefresh(int mode, decimal[] setDate){
-            if (mode == CALENDAR_MODE) return $"SELECT sethour, setminute, text, active FROM calendarlist WHERE year = {setDate[0]} AND month = {setDate[1]} AND day = {setDate[2]} ORDER BY sethour, setminute ASC;";
+        public string sqlListViewRefresh(int mode, decimal[] dateYMD){
+            if (mode == CALENDAR_MODE) return $"SELECT sethour, setminute, text, active FROM calendarlist WHERE year = {dateYMD[0]} AND month = {dateYMD[1]} AND day = {dateYMD[2]} ORDER BY sethour, setminute ASC;";
             else if (mode == ALARM_MODE) return "SELECT sethour, setminute, text FROM t_alarmlist ORDER BY sethour, setminute ASC;";
-            else return null;
+            return null;
         }
 
         // LISTBOX REFRESH          
-        public string sqlListboxRefresh(decimal[] date) {
-            return $"SELECT text FROM calendarlist WHERE year = {date[0]} AND month = {date[1]} AND day = {date[3]} ORDER BY sethour, setminute ASC";
+        public string sqlListboxRefresh(decimal[] dateYMD) {
+            return $"SELECT text FROM calendarlist WHERE year = {dateYMD[0]} AND month = {dateYMD[1]} AND day = {dateYMD[3]} ORDER BY sethour, setminute ASC";
+        }
+
+        // UPDATE SQL               
+        public string sqlUpdateData(int mode, decimal[] pastDate, decimal[] pastTime, decimal[] setTime, string text, bool alarmEnable) {
+            if (mode == CALENDAR_MODE) return $"UPDATE calendarlist SET (sethour, setminute, text, active) = ({setTime[0]}, {setTime[1]}, '{text}', {alarmEnable}) " +
+                                              $"WHERE year = {pastDate[0]} AND month = {pastDate[1]} AND day = {pastDate[2]} AND sethour = {pastTime[0]} AND setminute = {pastTime[1]};";
+
+            else if (mode == ALARM_MODE) return $"UPDATE t_alarmlist SET (sethour, setminute, text) = ({setTime[0]}, {setTime[1]}, '{text}') " +
+                                                $"WHERE sethour = {pastTime[0]} AND setminute = {pastTime[1]};";
+
+            return null;
+        }
+
+        // OVERLAP CHECK            
+        public string sqlOverlapCheck(int mode, decimal[] dateYMD, decimal[] time) {
+            if (mode == CALENDAR_MODE) return $"SELECT * FROM calendarlist WHERE year = {dateYMD[0]} AND month = {dateYMD[1]} AND day = {dateYMD[2]} AND sethour = {time[0]} AND setminute = {time[1]};";
+            else if (mode == ALARM_MODE) return $"SELECT * FROM t_alarmlist WHERE sethour = {time[0]} AND setminute = {time[1]};";
+            return null;
+        }
+
+        // DELEDE DATA              
+        public string sqlDeleteData(int mode, decimal[] dateYMD, decimal[] time) {
+            if (mode == CALENDAR_MODE) return $"SELECT FROM calendarlist WHERE year = {dateYMD[0]} AND month = {dateYMD[1]} AND day = {dateYMD[2]} AND sethour = {time[0]} AND setminute = {time[1]};";
+            else if (mode == ALARM_MODE) return $"DELETE FROM t_alarmlist WHERE sethour = {time[0]} AND setminute = {time[1]};";
+            return null;
+        }
+
+        // NEXT ALARM               
+        public string sqlNextAlarmCheck(decimal[] dateYMD) {
+            return $"SELECT * FROM calendarlist WHERE year >= {dateYMD[0]} AND month >= {dateYMD[1]} AND day >= {dateYMD[2]} ORDER BY year, month, day, sethour, setminute ASC;";
         }
     }
 }
